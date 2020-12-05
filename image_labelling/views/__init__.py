2e802d6cbbd3c46da3bfc22dadab309f5fc569ce
Wetula <73251035+Wetula@users.noreply.github.com>
2020-12-05 15:06:46 +0100

    Fixing user loader and adding admin requirement decorator

--- image_labelling/views/__init__.py
+++ image_labelling/views/__init__.py
@@ -1,4 +1,6 @@
-from .. import login_manager
+from .. import login_manager, db
+from ..database import User
+
 
 from .home import home
 from .users import users
@@ -9,10 +11,21 @@ blueprints = [home, auth, users, upload]
 
 
 @login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
+def load_user(id):
+    user = db.Query.get(id)
+    return user
 
 
 @login_manager.unauthorized_handler
 def unauthorized_callback():
     return redirect(url_for('login'))
+
+
+def admin_required(func):
+    @functools.wraps(func)
+    def _admin_required(*args, **kw):
+        admin = current_user.is_authenticated and current_user.is_admin
+        if not admin:
+            return app.login_manager.unauthorized()
+        return func(*args, **kw)
+    return _admin_required

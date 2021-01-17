7c8054b71aa75f9584c02e1226c8aff167b16f39
Wetula <73251035+Wetula@users.noreply.github.com>
2021-01-17 15:41:02 +0100

    Update __init__.py

--- image_labelling/views/__init__.py
+++ image_labelling/views/__init__.py
@@ -1,21 +1,25 @@
 from .. import login_manager, db
-from ..database import User
 from flask import redirect, url_for
 
 from .home import home
 from .users import users
 from .auth import auth
 from .upload import upload
+from .label import label
+from .create_polygon import create_polygon
+from .send_files import send_file
+from .howto import howto
 
-blueprints = [home, auth, users, upload]
-
-
-@login_manager.user_loader
-def load_user(id):
-    user = db.Query.get(id)
-    return user
+blueprints = [home, auth, users, upload, label, create_polygon, send_file, howto]
 
 
-@login_manager.unauthorized_handler
-def unauthorized_callback():
-    return redirect(url_for('auth.login'))
+# @login_manager.user_loader
+# def load_user(_id):
+#     user = db.Query.get(_id)
+#     return user
+#
+#
+# @login_manager.unauthorized_handler
+# def unauthorized_callback():
+#     print("Unauthorized")
+#     return redirect(url_for('auth.login'))

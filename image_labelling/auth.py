70fec9ff3f2cb721b157880ff5f8e6c8166b6e69
Wetula <73251035+Wetula@users.noreply.github.com>
2021-01-17 15:41:11 +0100

    Update auth.py

--- image_labelling/auth.py
+++ image_labelling/auth.py
@@ -3,7 +3,7 @@ from flask_login import current_user, LoginManager
 from image_labelling.database import User
 # from flask import current_app
 from . import login_manager
-from flask import redirect
+from flask import redirect, url_for, flash
 
 
 def admin_required(func):
@@ -14,3 +14,19 @@ def admin_required(func):
             return login_manager.unauthorized()
         return func(*args, **kw)
     return _admin_required
+
+
+@login_manager.user_loader
+def load_user(_id):
+    if _id is not None:
+        print("User was "+_id)
+        return User.query.get(_id)
+    print("User was "+ _id)
+    return None
+
+
+@login_manager.unauthorized_handler
+def unauthorized_callback():
+    print("Unauthorized")
+    flash('You must be logged in to view that page.')
+    return redirect(url_for('auth.login'))

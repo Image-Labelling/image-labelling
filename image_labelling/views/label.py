3d801736f72b0755a8b6d5ee3e6154db6db9fdfd
Wetula <73251035+Wetula@users.noreply.github.com>
2021-01-19 23:42:17 +0100

    Skeleton for changing label assigned to segmentation

--- image_labelling/views/label.py
+++ image_labelling/views/label.py
@@ -11,9 +11,22 @@ label = Blueprint('label', __name__)
 def add_label():
     form = LabelForm()
     if request.method == 'POST' and form.validate():
-            parent, name, language = form.data.parent, form.data.name, form.data.language
-            new_label = Label()
-            form.populate_obj(new_label)
-            return redirect('/label')
+        parent, name, language = form.data.parent, form.data.name, form.data.language
+        new_label = Label()
+        form.populate_obj(new_label)
+        return redirect('/label')
 
     return render_template('label.html', form=form)
+
+
+@label.route('/label_change')
+def label_change():
+    if 'segmentation_id' not in request.args:
+        return 'Malformed request'
+
+    if 'vote' in request.args and request.args.get('vote') == 'yes':
+        return 'Vote'
+    if 'create_new' in request.args and request.args.get('create_new') == 'yes':
+        return 'Make new'
+
+    return '500 BAD PATH'

6a49dcfc552ed8a31c797dd58722c6270e72af8c
Wetula <73251035+Wetula@users.noreply.github.com>
2021-01-25 22:29:48 +0100

    Group creation

--- image_labelling/views/__init__.py
+++ image_labelling/views/__init__.py
@@ -1,5 +1,6 @@
 from .auth import auth
 from .create_polygon import create_polygon
+from .group import group
 from .home import home
 from .howto import howto
 from .image import image
@@ -9,4 +10,4 @@ from .send_files import send_file
 from .upload import upload
 from .users import users
 
-blueprints = [home, auth, users, upload, label, create_polygon, send_file, howto, profile, image]
+blueprints = [home, auth, users, upload, label, create_polygon, send_file, howto, profile, image, group]
--- image_labelling/views/group.py
+++ image_labelling/views/group.py
@@ -1,18 +1,40 @@
-from flask import request, Blueprint, render_template, redirect
+from flask import request, Blueprint, redirect, url_for, flash
 
-from image_labelling.database import Label
-from image_labelling.form import LabelForm
+from ..database import Image, ImageGroup, db
 
 group = Blueprint('group', __name__)
 
-@group.route("/group", methods=['GET', 'POST'])
-def add_label():
-    form = LabelForm()
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            parent, name, language = form.data['parent'], form.data['name'], form.data['language']
-            new_label = Label()
-            form.populate_obj(new_label)
-            return redirect('/label')
 
-    return render_template('label.html', form=form)
+@group.route("/group_create")
+def add_group():
+    if 'this' in request.args and 'that' in request.args:
+        _this = request.args.get('this')
+        _that = request.args.get('that')
+
+        _this_entry = Image.query.filter(Image.id == _this).first()
+        _that_entry = Image.query.filter(Image.id == _that).first()
+
+        _this_group = _this_entry.group_id
+        _that_group = _that_entry.group_id
+
+        if _this_group is not None:
+            _group_id = _this_group
+        else:
+            if _that_group is not None:
+                _group_id = _that_group
+            else:
+                _group_id = None
+
+        if _group_id is not None:
+            _this_entry.group_id = _group_id
+            _that_entry.group_id = _group_id
+            db.session.commit()
+        else:
+            _group = ImageGroup()
+            db.session.add(_group)
+            db.session.commit()
+            _this_entry.group_id = _group.id
+            _that_entry.group_id = _group.id
+            db.session.commit()
+    flash("Group added.")
+    return redirect(request.referrer or url_for("home.index"))

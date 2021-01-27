e4ede7b6773064e237fdcae68f4d071e8800e817
Wetula <73251035+Wetula@users.noreply.github.com>
2021-01-27 15:03:21 +0100

    User model cleanup

--- image_labelling/database.py
+++ image_labelling/database.py
@@ -18,15 +18,12 @@ class User(UserMixin, db.Model):
                          unique=True, nullable=False)
     password = db.Column(db.Unicode(128))
     password_active = db.Column(db.Boolean, unique=False, default=True)
-    # _active = db.Column(db.Boolean, unique=False, default=False)
     _admin = db.Column(db.Boolean, unique=False, default=False)
-    # _anonymous = db.Column(db.Boolean, unique=False, default=False)
     registration_date = db.Column(
         db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         super(User, self).__init__(*args, **kwargs)
-        # self._authenticated = False
 
     def set_password(self, password):
         self.password = bcrypt.hashpw(
@@ -35,23 +32,6 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return bcrypt.checkpw(password.encode(
             'utf-8'), self.password)
-
-    # @property
-    # def is_authenticated(self):
-    #     return self._authenticated
-
-    # def authenticate(self, password):
-    #     check = self.verify_password(password)
-    #     self._authenticated = check
-    #     return self._authenticated
-
-    # @property
-    # def is_active(self):
-    #     return self._active
-
-    # @property
-    # def is_anonymous(self):
-    #     return self._anonymous
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
